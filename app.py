a5965bdcbb7637e483eebbba4b9ad60c8916a997
rahulharikumarr <52792591+rahulharikumarr@users.noreply.github.com>
2021-10-16 12:39:22 +0530

    app.py
    Main file

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+st.title("IRIS API")
+
+
+
+# Webpage
+sl = st.slider('Sepal Length', 4.3, 7.9, 5.1)
+sw = st.slider('Sepal Width', 4.5, 3.5, 2.2)
+pl = st.slider('Petal Length', 1.0, 6.9, 5.5)
+pw = st.slider('Petal Width', 0.1, 2.5, 2.2)
+
+
+
+# Model
+from sklearn.datasets import load_iris
+iris=load_iris()
+# iris.keys()
+from sklearn.tree import DecisionTreeClassifier
+model=DecisionTreeClassifier()
+model.fit(iris.data,iris.target)
+output=model.predict([[sl,sw,pl,pw]])
+output=iris.target_names[output[0]]
+st.title(f'The flower species is {output}')
